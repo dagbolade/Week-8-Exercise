186081fdd0af3ed32479e2ec1d648e7a0e8e4534
David Damilola <dagbolade72@gmail.com>
2023-11-14 11:27:08 +0000

    adding some hyperparameters to the grid search

--- Week-8 Exercise/GridSearchSample.py
+++ Week-8 Exercise/GridSearchSample.py
@@ -11,14 +11,15 @@ from sklearn import svm, datasets
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+
 iris = datasets.load_iris()
 X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.55, random_state=42)
-parameters = {'kernel':('linear', 'rbf'), 'C':[1, 10]}
+parameters = {'kernel': ('linear', 'rbf'), 'C': [1, 10]}
 svc = svm.SVC(C=10, kernel='rbf')
-grid = GridSearchCV(svc, parameters,cv=10, scoring='accuracy', return_train_score=False,verbose=1)
-grid.fit(X_train, y_train)
-print("Best parameters found: ", grid.best_params_)
-accuracy = grid.best_score_ *100
-print("Training accuracy for our training dataset with tuning is: {:.2f}%".format(accuracy))
-pred = grid.predict(X_test)
-print("Test accuracy using: {:.2f}% ".format(accuracy_score(pred, y_test)*100))
+# grid = GridSearchCV(svc, parameters,cv=10, scoring='accuracy', return_train_score=False,verbose=1)
+svc.fit(X_train, y_train)
+# print("Best parameters found: ", svc.best_params_)
+# accuracy = svc.best_score_ *100
+# print("Training accuracy for our training dataset with tuning is: {:.2f}%".format(accuracy))
+pred = svc.predict(X_test)
+print("Test accuracy using: {:.2f}% ".format(accuracy_score(pred, y_test) * 100))
